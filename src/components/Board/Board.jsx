6dfc3c96633d9d7f021999e5f41c02ea3e61ed8b
James Easter <james.m.easter@gmail.com>
2020-08-06 16:17:33 -0500

    (feat) add Board component

---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Board.css";
+
+const Board = () => {
+  return (
+    <div className="board">
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+    </div>
+  );
+};
+
+export default Board;
