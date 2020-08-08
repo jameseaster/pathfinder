63a4d1aa7af93e450db23ec6f59e2195e9c1306e
James Easter <james.m.easter@gmail.com>
2020-08-07 19:33:40 -0500

    (feat) pass generated matrix to Board component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import Menu from "./components/Menu/Menu";
 import Board from "./components/Board/Board";
-
+import generateMatrix from "./Matrix";
 function App() {
+  const matrix = generateMatrix();
   return (
     <div>
       <Menu />
-      <Board />
+      <Board matrix={matrix} />
     </div>
   );
 }
